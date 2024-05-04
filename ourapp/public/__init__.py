a05416bb1da1e11a5baeee191ef5924f626924cf
venkata-sandeep-addala <168465210+venkata-sandeep-addala@users.noreply.github.com>
2024-05-04 21:37:03 +0530

    trending, latest products

--- ourapp/public/__init__.py
+++ ourapp/public/__init__.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from ourapp.models import Product
 
 public = Blueprint("public", __name__, template_folder="templates", url_prefix="/")
 
 @public.route("/")
 def index():
-    return render_template("public/home.html")
+    trending_products=Product.query.all()
+    print(trending_products)
+    return render_template("public/home.html",trending_products=trending_products)
+
