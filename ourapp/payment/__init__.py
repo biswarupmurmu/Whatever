96669c2863159576257019312bf40afc5a3de2a0
venkata-sandeep-addala <168465210+venkata-sandeep-addala@users.noreply.github.com>
2024-05-07 00:32:04 +0530

    Implemented Logging functionality

--- ourapp/payment/__init__.py
+++ ourapp/payment/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, session, url_for
 from flask_login import current_user, login_required
 from ourapp.payment.form import PaymentForm
+from ourapp.logging_config.config import logger
 
 payment_bp=Blueprint("payment",__name__,template_folder="templates",url_prefix="/payment")
 
@@ -9,9 +10,11 @@ payment_bp=Blueprint("payment",__name__,template_folder="templates",url_prefix="
 def payment():
     cart=current_user.cart
     if len(cart) == 0:
+        logger.info("Redirecting to view cart because the cart is empty for %s(%s).",current_user.fname, current_user.id)
         return redirect(url_for("cart.view_cart"))
     if not current_user.address:
         flash(message="Address required", category="info")
+        logger.info("Redirecting to view cart because the user %s(%s) has no address.",current_user.fname, current_user.id)
         return redirect(url_for("cart.view_cart"))
     cart_total = 0
     for cartItem in cart:
@@ -21,6 +24,7 @@ def payment():
 
     if form.validate_on_submit():
         session["payment_received"] = True
+        logger.info("Payment received. Redirecting to place order.")
         return redirect(url_for("order_bp.place_order"))
-
+    logger.info("Rendering payment page.")
     return render_template("payment/payment.html",form = form, cart_total=cart_total)
--- ourapp/product/__init__.py
+++ ourapp/product/__init__.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for
 from ourapp.models import Product, Category
+from ourapp.logging_config.config import logger
 
 product_bp=Blueprint("product_bp",__name__, url_prefix="/product", template_folder="templates")
 
 @product_bp.route('/all')
 def view_all_products():
     products=Product.query.all()
+    logger.info("Viewing all products.")
     return render_template("product/all.html",products=products)
 
 # View products by categories
@@ -14,12 +16,15 @@ def view_products_by_category(category):
     category = Category.query.filter(Category.name.ilike(category)).first()
     if category:
         products_with_categories=category.products.all()
+        logger.info("Viewing products by category: %s", category)
         return render_template("product/products_by_categories.html",products=products_with_categories, category=category.name)
     else:
+        logger.warning("Category not found: %s. Redirecting to view all products.", category)
         return redirect(url_for('product_bp.view_all_products'))
 
 @product_bp.route('/<int:id>')
 def view_product_details(id):
     product = Product.query.filter_by(id=id).first()
     features=product.features.split('\n')
+    logger.info("Viewing details of product %s(%s).", product.name, product.id)
     return render_template("product/product_details.html", product=product,features=features)
